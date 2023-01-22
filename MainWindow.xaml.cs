da0ecb0ed0198f024f88e982e71253e937d622cb
kernel <86926580+kernelboy34@users.noreply.github.com>
2023-01-22 13:48:17 -0400

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -39,7 +39,7 @@ namespace encriptacion
             collatz iam = new collatz();
             iam.sec(fj);
             iam.proceso(a);
-
+           //hecho por kernelboy34
         }
     }
 }
